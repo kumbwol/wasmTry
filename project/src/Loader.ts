45a782f2df500d1ea47137a0e1bbe996a322c716
jakabwolosz <jakab.wolosz@playngo.com>
2021-11-23 21:41:12 +0100

    add compress image

--- project/src/Loader.ts
+++ project/src/Loader.ts
@@ -6,6 +6,7 @@ export class Loader
 	{
 		loader
 			.add("image0", "./project/assets/image0.png")
+			.add("image1", "./project/assets/image1.png")
 			.load(() => callback.setup());
 	}
 }
--- project/src/game/GameManager.ts
+++ project/src/game/GameManager.ts
@@ -4,14 +4,16 @@ export class GameManager
 {
 	constructor(mainContainer: PIXI.Container)
 	{
-		console.log("Here comes the game");
-
 		let imageTexture = PIXI.Texture.from("image0");
 		let imageSprite = new PIXI.Sprite(imageTexture);
 		imageSprite.x = 200;
 		imageSprite.y = 200;
-
-
 		mainContainer.addChild(imageSprite);
+
+		let compressedImageTexture = PIXI.Texture.from("image1");
+		let compressedImageSprite = new PIXI.Sprite(compressedImageTexture);
+		compressedImageSprite.x = 600;
+		compressedImageSprite.y = 200;
+		mainContainer.addChild(compressedImageSprite);
 	}
 }
